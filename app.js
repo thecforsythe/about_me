fc065e9404217c45729a23be55999fd4aa94c970
Dustin <dyelland@gmail.com>
2017-06-07 21:17:25 -0500

    fixed bugs(nested functions), added y/n as viable answers

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 'use strict';
 
-var game = confirm('Would you like to play a game?');
+var userName = prompt('What is your name?');
+console.log('User name is: ' + userName);
+var game = confirm('Welcome ' + userName + '! Would you like to play a game?');
+var questions = ['Am I 20 years old?',
+  'Did I used to be an electrician?',
+  'Is DeltaV my first exposure to programming?',
+  'Do I consider myself a morning person?',
+  'Do I have 6 siblings?',
+  'What is my lucky number?',
+  'Name one of the two sports I enjoy the most.'
+];
+var userGuesses = [];
+var answerCorrect = ['no', //Answer 1
+  'yes', //Answer 2
+  'no', //Answer 3
+  'no', //Answer 4
+  'yes', //Answer 5
+  '7', //Answer 6
+  'running', //Answer 7
+  'frisbee' //Also answer 7
+];
+var userGuessesLowerCase = [];
+var score = 0;
 
 while (game) {
   runGame();
 }
 
 function runGame() {
-  var questions = ['Am I 20 years old?',
-    'Did I used to be an electrician?',
-    'Is DeltaV my first exposure to programming?',
-    'Do I consider myself a morning person?',
-    'Do I have 6 siblings?',
-    'What is my lucky number?',
-    'Name one of the two sports I enjoy the most.'
-  ];
-  var userGuesses = [];
-  var answerCorrect = ['no', //Answer 1
-    'yes', //Answer 2
-    'no', //Answer 3
-    'no', //Answer 4
-    'yes', //Answer 5
-    '7', //Answer 6
-    'running', //Answer 7
-    'frisbee' //Also answer 7
-  ];
-
-  var userName = prompt('First, what is your name?');
-
-  console.log('User name is: ' + userName);
-
   //Loop displays questions and collects answer
   for (var i = 0; i < questions.length; i++) {
     userGuesses[i] = prompt(questions[i]);
@@ -38,42 +37,47 @@ function runGame() {
   }
 
   getLowerCase();
+  normalizeAnswers();
   checkAnswer();
   displayScore();
+  game = confirm('Thank you ' + userName + ' for playing. Would you like to play again?');
+}
 
-  var userGuessesLowerCase = [];
-  function getLowerCase() {
-    for (var j = 0; j < questions.length; j++) {
-      if (userGuesses[j] !== null) {
-        userGuessesLowerCase[j] = userGuesses[j].toLowerCase();
-      } else {
-        return userGuessesLowerCase[j] = '';
-      }
+function getLowerCase() {
+  for (var j = 0; j < questions.length; j++) {
+    if (userGuesses[j] !== null) {
+      userGuessesLowerCase[j] = userGuesses[j].toLowerCase();
     }
-    return userGuessesLowerCase;
   }
+}
+
+function normalizeAnswers() {
+  for(var i = 0; i < questions.lenghth; i++) {
+    if (userGuesses[i] === 'y') {
+      userGuesses[i] = 'yes';
+    }
+    if (userGuesses[i] === 'n') {
+      userGuesses[i] === 'no';
+    }
+  }
+}
 
 //This function checks user answer is true or false, then returns out a score
-  function checkAnswer(userGuessesLowerCase, answerCorrect) {
-    var isAnswerCorrect = [];
-    var score = 0;
-    for(var i = 0; i <= questions.length; i++) {
-      console.log('userGuessesLowerCase');
-      if (userGuessesLowerCase[i] === answerCorrect[i]) {
-        isAnswerCorrect = true;
-        console.log('Answer #' + (i + 1) + ' is correct.');
-        return score++;
-      }
-      else {
-        isAnswerCorrect = false;
-        console.log('Answer #' + (i + 1) + ' is incorrect.');
-      }
+function checkAnswer() {
+  console.log(userGuessesLowerCase);
+  console.log(answerCorrect);
+  var score = 0;
+  for(var i = 0; i < questions.length; i++) {
+    if (userGuessesLowerCase[i] == answerCorrect[i] ||
+    userGuessesLowerCase[6] === answerCorrect[7]) {
+      console.log('Answer #' + (i + 1) + ' is correct.');
+      score++;
+    } else {
+      console.log('Answer #' + (i + 1) + ' is incorrect.');
     }
-    return score;
   }
+}
 
-  function displayScore(score) {
-    alert('You got ' + score + ' questions correct!');
-  }
-  game = confirm('Thank you ' + userName + ' for playing. Would you like to play again?');
+function displayScore() {
+  alert('You got ' + score + ' questions correct!');
 }
